c8dd38ef34a3831e02aa6ece62bc70b0fbef4b65
dazes97 <danielzeballos97@gmail.com>
2024-11-25 22:49:49 -0400

    chore: initial setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
+import {
+  BrowserAnimationsModule,
+  provideAnimations,
+} from '@angular/platform-browser/animations';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
