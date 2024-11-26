c9ba562fdd1de77c4b0c07a39f5cf88db08b2ee7
dazes97 <danielzeballos97@gmail.com>
2024-11-25 23:11:18 -0400

    feat: FE-Crear pantalla de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from '@modules/log-in/log-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LogInComponent },
+];
